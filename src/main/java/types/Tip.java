767160c668fad4c4d3808cf5d0a38d78515ebff1
Arnaud Weyts <arnaud.weyts@gmail.com>
2017-12-07 09:49:57 +0100

    add tip type

---
+++ src/main/java/types/Tip.java
@@ -0,0 +1,16 @@
+package types;
+
+public class Tip {
+  private int id;
+  private String picture;
+  private String tip_content;
+  private String rich_language;
+  private String circumstances;
+  private String ZILL_goal;
+  private String development_goal;
+  private int themes_id;
+
+  public Tip() {
+
+  }
+}
