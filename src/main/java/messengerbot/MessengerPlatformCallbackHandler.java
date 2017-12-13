4d24c47e0aff731205202e0636d55bad95921bea
Daan Zwaenepoel <daandotninja@users.noreply.github.com>
2017-12-13 16:05:34 +0100

    work pls aboneer debug

--- src/main/java/messengerbot/MessengerPlatformCallbackHandler.java
+++ src/main/java/messengerbot/MessengerPlatformCallbackHandler.java
@@ -204,7 +204,7 @@ public class MessengerPlatformCallbackHandler {
         final QuickReply.ListBuilder quickReplies = QuickReply.newListBuilder();
         try{
             this.sendClient.sendTextMessage(senderId, "voor de foreach werk ik nog");
-            this.sendClient.sendTextMessage(senderId, requestHandler.GetAgeGroups().toString());
+            this.sendClient.sendTextMessage(senderId, requestHandler.GetAgeGroups().get(0).getGroup_name());
             requestHandler.GetAgeGroups().forEach(ageGroup -> quickReplies.addTextQuickReply(ageGroup.getGroup_name(),Integer.toString(ageGroup.getId())).toList());
 //            quickReplies.build();
 
