c45a0d607216c7324c7fe3e7c8f35210d4ba5753
Daan Zwaenepoel <daandotninja@users.noreply.github.com>
2017-12-14 07:43:46 +0100

    help work pls

--- src/main/java/messengerbot/MessengerPlatformCallbackHandler.java
+++ src/main/java/messengerbot/MessengerPlatformCallbackHandler.java
@@ -147,13 +147,14 @@ public class MessengerPlatformCallbackHandler {
     }
 
     private void sendHelp(String recipientId) throws MessengerApiException, MessengerIOException {
-        final  List<com.github.messenger4j.send.buttons.Button> buttons = com.github.messenger4j.send.buttons.Button.newListBuilder()
-                .addPostbackButton("Uitschrijven", "uitschrijven").toList()
-                .addPostbackButton("Tip", "tip").toList()
-                .addPostbackButton("ben ik al geregistreerd", "aboneer").toList()
-                .build();
-        final ButtonTemplate buttonTemplate = ButtonTemplate.newBuilder("Probeer een van volgende commando's", buttons).build();
-        this.sendClient.sendTemplate(recipientId, buttonTemplate);
+
+                   final  List<com.github.messenger4j.send.buttons.Button> buttons = com.github.messenger4j.send.buttons.Button.newListBuilder()
+                            .addPostbackButton("Aboneer", "aboneer").toList()
+                           .addPostbackButton("Uitschrijven", "uitschrijven").toList()
+                           .addPostbackButton("Tip", "tip").toList()
+                           .build();
+            final ButtonTemplate buttonTemplate = ButtonTemplate.newBuilder("Tap a button", buttons).build();
+            this.sendClient.sendTemplate(recipientId, buttonTemplate);
         //        sendHelpSub(recipientId);
 //        try{
 //
