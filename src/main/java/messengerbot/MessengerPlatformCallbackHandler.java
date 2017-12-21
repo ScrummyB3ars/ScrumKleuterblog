be63a3d5b6f78756eb908e929fecafb7ab0eaf15
Daan Zwaenepoel <daandotninja@users.noreply.github.com>
2017-12-21 08:31:55 +0100

    interaction tip

--- src/main/java/messengerbot/MessengerPlatformCallbackHandler.java
+++ src/main/java/messengerbot/MessengerPlatformCallbackHandler.java
@@ -224,7 +224,7 @@ public class MessengerPlatformCallbackHandler {
                 .buttons(buttons)
                 .toList()
 
-                .addElement("Betrek kleuters op elkaar: wat vind jij van wat Ali zegt?")
+                .addElement("Betrek kleuters op elkaar: wat vind...")
                 .subtitle("...")
                 .imageUrl("https://www.onlineseminar.nl/media/1244/ols-tip-1.png")
                 .buttons(buttons)
