217f65da74db55159d45bd205aaa78e136fe9019
Daan Zwaenepoel <daandotninja@users.noreply.github.com>
2017-12-21 08:21:59 +0100

    interaction tip

--- src/main/java/messengerbot/MessengerPlatformCallbackHandler.java
+++ src/main/java/messengerbot/MessengerPlatformCallbackHandler.java
@@ -204,7 +204,7 @@ public class MessengerPlatformCallbackHandler {
     }
     private void sendBuildTip(String senderId,ThemeTip themeTip,InteractionTip interactionTip)throws MessengerApiException, MessengerIOException{
         String themeTipImgUrl = "https://api-toddlr.herokuapp.com/images/"+themeTip.getPicture();
-        String  titleInteraction = interactionTip.getTip_content().substring(0,77);
+        String  titleInteraction = "hellos".substring(0,77);
         final List<com.github.messenger4j.send.buttons.Button> buttons = Button.newListBuilder()
                 .addUrlButton("Beoordeel tip", "https://docs.google.com/forms/d/e/1FAIpQLSekeCPYI_OxUHBOnRPorjyY6BXlMACZmXz2S2OiEYhQIxUSXw/viewform").toList()
                 .addUrlButton("Bekijk tip", "https://docs.google.com/forms/d/e/1FAIpQLSekeCPYI_OxUHBOnRPorjyY6BXlMACZmXz2S2OiEYhQIxUSXw/viewform").toList()
@@ -224,7 +224,7 @@ public class MessengerPlatformCallbackHandler {
                 .buttons(buttons)
                 .toList()
 
-                .addElement("...")
+                .addElement(titleInteraction)
                 .subtitle("...")
                 .imageUrl("https://www.onlineseminar.nl/media/1244/ols-tip-1.png")
                 .buttons(buttons)
